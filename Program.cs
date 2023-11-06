1ac4b5e58e09559c8f112cdbda269d99448da668
Simon Rohrer <simon.rohrer@live.com>
2023-11-06 11:07:47 +0000

    Commit

--- Program.cs
+++ Program.cs
@@ -1,45 +1,15 @@
-﻿string? readResult;
-int startIndex = 0;
-bool goodEntry = false;
+﻿/*  
+This code instantiates a value and then calls the ChangeValue method
+to update the value. The code then prints the updated value to the console.
+*/
+int x = 5;
 
-int[] numbers = { 1, 2, 3, 4, 5 };
+x = ChangeValue(x);
 
-// Display the array to the console.
-Console.Clear();
-Console.Write("\n\rThe 'numbers' array contains: { ");
-foreach (int number in numbers)
+Console.WriteLine(x);
+
+int ChangeValue(int value) 
 {
-    Console.Write($"{number} ");
-}
-
-// To calculate a sum of array elements, 
-//  prompt the user for the starting element number.
-Console.WriteLine($"}}\n\r\n\rTo sum values 'n' through 5, enter a value for 'n':");
-while (goodEntry == false)
-{
-    readResult = Console.ReadLine();
-    goodEntry = int.TryParse(readResult, out startIndex);
-
-    if (startIndex > 5)
-    {
-        goodEntry = false;
-        Console.WriteLine("\n\rEnter an integer value between 1 and 5");
-    }
-}
-
-// Display the sum and then pause.
-Console.WriteLine($"\n\rThe sum of numbers {startIndex} through {numbers.Length} is: {SumValues(numbers, startIndex)}");
-
-Console.WriteLine("press Enter to exit");
-readResult = Console.ReadLine();
-
-// This method returns the sum of elements n through 5
-static int SumValues(int[] numbers, int n)
-{
-    int sum = 0;
-    for (int i = n; i < numbers.Length; i++)
-    {
-        sum += numbers[i];
-    }
-    return sum;
+    value = 10;
+    return value;
 }
